9eab0519bdc9f83d2fb1c0109631bfa1be369030
Meet Banthia <meetbanthia@Meets-MacBook-Pro.local>
2025-04-30 16:53:13 +0530

    Add implementation of flow network flownetwork.ipynb. dksup.ipynb is temporarily not required. given.py declares given variables and this is used everywhere to access them. output.txt stores value of R_star. verification that algorithm is working as intended is still left.

---
+++ given.py
@@ -0,0 +1,8 @@
+clients1 = [[0, 0], [1, 2]]      # Clients at time step 1
+clients2 = [[5, 5], [6, 6]]      # Clients at time step 2
+
+facilities1 = [[0, 1], [2, 2], [3, 0]]  # Facilities at time 1
+facilities2 = [[5, 6], [6, 5], [4, 4]]  # Facilities at time 2
+
+k = 2  # Number of facilities to open per time step
+B = 10  # Movement bound between facilities
