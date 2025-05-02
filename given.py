32f5ead3a13eb10d6b04ae4c38f19127aced76e5
Meet Banthia <meetbanthia@Meets-MacBook-Pro.local>
2025-05-02 14:14:23 +0530

    Fix issues in ropt.ipynb, had issue when a facility does not have any edges Modified example test case, works properly

--- given.py
+++ given.py
@@ -1,8 +1,10 @@
-clients1 = [[0, 0], [1, 2]]      # Clients at time step 1
-clients2 = [[5, 5], [6, 6]]      # Clients at time step 2
+clients1 = [[0, 0], [2, 2], [3, 1], [5, 5], [6, 3], [7, 2], [8, 4], [9, 0]]
+clients2 = [[1, 1], [4, 2], [5, 0], [6, 6], [7, 4], [8, 2], [9, 3], [10, 1]]
 
-facilities1 = [[0, 1], [2, 2], [3, 0]]  # Facilities at time 1
-facilities2 = [[5, 6], [6, 5], [4, 4]]  # Facilities at time 2
+facilities1 = [[0, 1], [1, 0], [2, 1], [3, 3], [4, 4], [5, 2], [6, 1], [7, 0]]
+facilities2 = [[1, 2], [3, 1], [4, 3], [5, 1], [6, 5], [7, 3], [8, 1], [9, 2]]
 
-k = 2  # Number of facilities to open per time step
-B = 10  # Movement bound between facilities
+B = 3  
+# Facility movement limit
+k = 3  
+# Number of facilities to open at each time
